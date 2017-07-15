855ef3034fb734ea3d1aea695ab7ca27f3896519
mbear0 <Mitchell.beare@computers4learning.org.au>
2017-07-15 10:28:19 +1000

    Intial Commit
    Intial commit

---
+++ Automated-QA-v001.ps1
@@ -0,0 +1,24 @@
+<#N
+Name of Script
+Purpose of Script
+Author
+Date Written
+#>
+
+
+
+
+#Global Variable Declarations
+
+#Function Declarations
+Function Get-Software{
+<#
+Sounds like Chad's Job
+
+#>
+
+
+}#End Get-Software
+
+
+#Main Code
