e7629adb9f4815aed2141551e5db6c5a8c50a8cf
mbear0 <Mitchell.beare@computers4learning.org.au>
2017-07-15 10:55:13 +1000

    Formatted Main Function
    Put basic logic in place for work.

--- Automated-QA-v001.ps1
+++ Automated-QA-v001.ps1
@@ -27,13 +27,32 @@ Function Test-DeviceDrivers{
 Function Set-Volume{
 
 }#End Set-Volume
+Function Test-Speakers{
+}#End Test-Speakers
 Function Expand-Drives{
 }#End Expand-Drives
 Function Start-Video{
 }#End Start-Video
+Function Invoke-CCleaner{
+}#End Invoke-CCleanerx
 #Main Code
 
+Write-Verbose 'Retrieving drives and expanding.'
+Expand-Drives
 
+Write-Verbose 'Contacting Registry and checking for Software'
+Get-Software 'Adobe Reader'
+Get-Software 'Firefox'
+Get-Software 'VLC Player'
+Get-Software 'Panda'
+
+Write-Verbose 'Setting Volume to maximum and testing'
+if(Set-Volume '100' -eq $true){
+    Test-Speakers
+}else{
+    #Give an error to report
+}
 <#
+start-process $msbuild $arguments 
 
 #>
