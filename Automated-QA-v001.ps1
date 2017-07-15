8b67491f79706b58e910b2d80b2900aad78307e7
mbear0 <Mitchell.beare@computers4learning.org.au>
2017-07-15 12:02:34 +1000

    Connected to Audio API

--- Automated-QA-v001.ps1
+++ Automated-QA-v001.ps1
@@ -14,25 +14,69 @@ Written By Mitchell Beare and Chad Gay
 #Global Variable Declarations
 
 #Function Declarations
-Function Get-Software($app) {
-
-$64bit = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Where-Object -FilterScript {$_.DisplayName -like "*$app*"} | Format-Table –AutoSize
-
- if ($64bit -eq $null){
-    $32bit = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Where-Object -FilterScript {$_.DisplayName -like "*$app*"} | Format-Table –AutoSize
-    $32bit
- } else {
-    $64bit
- }
-
-#Get-WmiObject -Class Win32_Product -ComputerName .| Select-Object name,version | Where-Object -FilterScript {$_.Name -like "*$app*"} | Format-List -Property *
-
+Function Get-Software{
+    param([string]$app)
+    Write-Verbose "Checking 64bit registry for $app."
+    $64bit = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Where-Object -FilterScript {$_.DisplayName -like "*$app*"} | Format-Table -AutoSize
+    if ($64bit -eq $null){
+    Write-Verbose "Checking 32bit registry for $app."
+        $32bit = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Where-Object -FilterScript {$_.DisplayName -like "*$app*"} | Format-Table -AutoSize
+        $32bit
+    }elseif($32bit -eq $null) {
+        Write-Error "$app Could not be found."
+    }else{
+        $64bit
+    }
 }#End Get-Software
 Function Test-DeviceDrivers{
 
 }#End Test-Drivers
 Function Set-Volume{
-
+    Param()
+    Add-Type -TypeDefinition @'
+    using System.Runtime.InteropServices;
+    [Guid("5CDF2C82-841E-4546-9722-0CF74078229A"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    interface IAudioEndpointVolume{
+        // f(), g(), ... are unused COM method slots. Define these if you care
+        int f(); int g(); int h(); int i();
+      int SetMasterVolumeLevelScalar(float fLevel, System.Guid pguidEventContext);
+      int j();
+      int GetMasterVolumeLevelScalar(out float pfLevel);
+      int k(); int l(); int m(); int n();
+      int SetMute([MarshalAs(UnmanagedType.Bool)] bool bMute, System.Guid pguidEventContext);
+      int GetMute(out bool pbMute);
+    }
+    [Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    interface IMMDevice{
+        int Activate(ref System.Guid id, int clsCtx, int activationParams, out IAudioEndpointVolume aev);
+    }
+    [Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    interface IMMDeviceEnumerator{
+        int f(); // Unused
+        int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice endpoint);
+    }
+    [ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")] class MMDeviceEnumeratorComObject { }
+    public class Audio{
+        static IAudioEndpointVolume Vol(){
+            var enumerator = new MMDeviceEnumeratorComObject() as IMMDeviceEnumerator;
+            IMMDevice dev = null;
+            Marshal.ThrowExceptionForHR(enumerator.GetDefaultAudioEndpoint(/*eRender*/ 0, /*eMultimedia*/ 1, out dev));
+            IAudioEndpointVolume epv = null;
+            var epvid = typeof(IAudioEndpointVolume).GUID;
+            Marshal.ThrowExceptionForHR(dev.Activate(ref epvid, /*CLSCTX_ALL*/ 23, 0, out epv));
+            return epv;
+        }
+        public static float Volume{
+            get { float v = -1; Marshal.ThrowExceptionForHR(Vol().GetMasterVolumeLevelScalar(out v)); return v; }
+            set { Marshal.ThrowExceptionForHR(Vol().SetMasterVolumeLevelScalar(value, System.Guid.Empty)); }
+        }
+    public static bool Mute
+    {
+        get { bool mute; Marshal.ThrowExceptionForHR(Vol().GetMute(out mute)); return mute; }
+        set { Marshal.ThrowExceptionForHR(Vol().SetMute(value, System.Guid.Empty)); }
+    }
+}
+'@
 }#End Set-Volume
 Function Test-Speakers{
 }#End Test-Speakers
