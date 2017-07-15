5d21ed4ab30c681913c041bcfec1aaf2c2db49ca
mbear0 <Mitchell.beare@computers4learning.org.au>
2017-07-15 11:25:00 +1000

    Wrote Expand-Drive Function

--- Automated-QA-v001.ps1
+++ Automated-QA-v001.ps1
@@ -30,6 +30,20 @@ Function Set-Volume{
 Function Test-Speakers{
 }#End Test-Speakers
 Function Expand-Drives{
+    $drives = Get-PSDrive -PSProvider FileSystem | Select-Object Name
+    foreach($drive in $drives){
+        $maxsize = (Get-PartitionSupportedSize -DriveLetter $drive).sixemax
+        $driveSize = (Get-PartitionSupportedSize -DriveLetter $drive).size
+         if($driveSize = $maxSize){
+            Write-Verbose "$drive Drive is already at maximum size"
+        }Else{
+            Resize-Partition -DriveLetter $drive -Size $MaxSize
+            Write-Verbose "Successfully expanded $drive Drive."
+        }
+        $MaxSize = $MaxSize/1024/1024/1024
+        $MaxSize = [Math]::Round($MaxSize)
+        $MaxSize = "HDD Size $mazsize(GB): " + $MaxSize
+    }
 }#End Expand-Drives
 Function Start-Video{
 }#End Start-Video
