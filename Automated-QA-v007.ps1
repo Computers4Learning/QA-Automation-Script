d52c15bc652e6051897f8cf9ebd5deaba805d44b
mbear0 <Mitchell.beare@computers4learning.org.au>
2017-08-19 09:40:56 +1000

    Script Self Removal

--- Automated-QA-v007.ps1
+++ Automated-QA-v007.ps1
@@ -189,23 +189,23 @@ Function Start-CCleaner {
   }
 }#End Start-CCleaner
 Function Set-ComputerIcon{
-#Registry key path 
-$path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" 
-#Property name 
-$name = "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" 
-#check if the property exists 
-$item = Get-ItemProperty -Path $path -Name $name -ErrorAction SilentlyContinue 
-if($item) 
-{ 
+  #Registry key path 
+  $path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" 
+  #Property name 
+  $name = "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" 
+  #check if the property exists 
+  $item = Get-ItemProperty -Path $path -Name $name -ErrorAction SilentlyContinue 
+  if($item) 
+  { 
     #set property value 
     Set-ItemProperty  -Path $path -name $name -Value 0  
-} 
-Else 
-{ 
+  } 
+  Else 
+  { 
     #create a new property 
     New-ItemProperty -Path $path -Name $name -Value 0 -PropertyType DWORD  | Out-Null  
     RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True
-} 
+  } 
 }#End Set-ComputerIcon
 
 #Main Code
@@ -234,3 +234,6 @@ Test-DeviceDrivers
 Read-Host 'QA Complete Computer will now Restart.'
 Restart-Computer -Force
 #End Main
+
+#Self Removal, must always be last line.
+Remove-Item $MyINvocation.InvocationName
